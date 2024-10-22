574daaf9ebef81fdec60e6b1725e3a5415e51b73
Cy Pokhrel <cy@cy7.sh>
2024-10-22 23:20:42 +0000

    get rid of dotenv

--- executable_borgbackup.sh
+++
@@ -1,7 +0,0 @@
-#/bin/env bash
-
-export GNUPGHOME=~/.gnupg/trezor
-export PASSWORD_STORE_DIR=~/.vault/password-store
-export BORG_PASSCOMMAND="pass show borg/ssd"
-
-borg create -sp --list --exclude-caches --exclude-from borgexclude.txt /mnt/veracrypt1/borgrepo::yt-home-{now:%Y-%m-%dT%H:%M} .
