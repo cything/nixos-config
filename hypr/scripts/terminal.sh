c10597732c3e3ef5ac07572bd6997601a3656b6a
Cy Pokhrel <cy@cy7.sh>
2024-11-26 17:26:20 -0500

    port my terminal script to hyprland

---
+++ hypr/scripts/terminal.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+focused_workspace=$(hyprctl activeworkspace -j | jq '.id')
+
+kitty_window_count=$(hyprctl clients -j | jq --argjson workspace "$focused_workspace" '[.[] |select(.workspace.id == $workspace and .class == "kitty")] |length')
+
+next_session=$((focused_workspace * 10))
+
+if [ "$kitty_window_count" -gt 0 ]
+then
+  next_session=$((next_session + kitty_window_count))
+fi
+
+kitty tmux new-session -A -s ${next_session}
