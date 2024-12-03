2ffef35935df2c7e90123288a2401899a9ff1c3c
cy <cy@cything.io>
2024-12-02 23:17:01 -0500

    use foot

--- hypr/scripts/terminal.sh
+++ hypr/scripts/terminal.sh
@@ -2,7 +2,7 @@
 
 focused_workspace=$(hyprctl activeworkspace -j | jq '.id')
 
-kitty_window_count=$(hyprctl clients -j | jq --argjson workspace "$focused_workspace" '[.[] |select(.workspace.id == $workspace and .class == "kitty")] |length')
+kitty_window_count=$(hyprctl clients -j | jq --argjson workspace "$focused_workspace" '[.[] |select(.workspace.id == $workspace and .class == "foot")] |length')
 
 next_session=$((focused_workspace * 10))
 
@@ -11,4 +11,4 @@ then
   next_session=$((next_session + kitty_window_count))
 fi
 
-kitty tmux new-session -A -s ${next_session}
+foot tmux new-session -A -s ${next_session}
