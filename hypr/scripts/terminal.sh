29ca55a44765f484bb1d174371d194b9d023eee3
cy <hi@cything.io>
2024-12-03 19:08:27 -0500

    rename kitty to foot

--- hypr/scripts/terminal.sh
+++ hypr/scripts/terminal.sh
@@ -2,13 +2,13 @@
 
 focused_workspace=$(hyprctl activeworkspace -j | jq '.id')
 
-kitty_window_count=$(hyprctl clients -j | jq --argjson workspace "$focused_workspace" '[.[] |select(.workspace.id == $workspace and .class == "foot")] |length')
+foot_window_count=$(hyprctl clients -j | jq --argjson workspace "$focused_workspace" '[.[] |select(.workspace.id == $workspace and .class == "foot")] |length')
 
 next_session=$((focused_workspace * 10))
 
-if [ "$kitty_window_count" -gt 0 ]
+if [ "$foot_window_count" -gt 0 ]
 then
-  next_session=$((next_session + kitty_window_count))
+  next_session=$((next_session + foot_window_count))
 fi
 
 foot tmux new-session -A -s ${next_session}
