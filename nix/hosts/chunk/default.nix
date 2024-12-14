63fca71c8c56cd4ee5d100132a1a0b10fd4a77bc
cy <hi@cything.io>
2024-12-14 16:47:58 -0500

    bring adguard here

--- nix/hosts/chunk/default.nix
+++ nix/hosts/chunk/default.nix
@@ -285,5 +285,21 @@ in {
       }
     ];
   };
+
+  # adguard
+  services.adguardhome = {
+    enable = true;
+    host = "127.0.0.1";
+    port = 8082;
+    settings = {
+      http.port = "8083";
+      users = [
+        {
+          name = "cy";
+          password = "$2y$10$BZy2zYJj5z4e8LZCq/GwuuhWUafL/MNFO.YcsAMmpDS.2krPxi7KC";
+        }
+      ];
+    };
+  };
 }
 
