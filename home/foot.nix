ef542c35cc857cae81908c02e6d6e905a789ae74
cy <hi@cything.io>
2024-12-25 03:37:28 -0500

    tmux and foot: use nightfox colorscheme

--- home/foot.nix
+++ home/foot.nix
@@ -23,28 +23,27 @@
       };
       # rose pine: https://codeberg.org/dnkl/foot/src/branch/master/themes/rose-pine
       colors = {
-        background = "191724";
-        foreground = "e0def4";
-
-        regular0 = "26233a"; # black (Overlay)
-        regular1 = "eb6f92"; # red (Love)
-        regular2 = "9ccfd8"; # green (Foam)
-        regular3 = "f6c177"; # yellow (Gold)
-        regular4 = "31748f"; # blue (Pine)
-        regular5 = "c4a7e7"; # magenta (Iris)
-        regular6 = "ebbcba"; # cyan (Rose)
-        regular7 = "e0def4"; # white (Text)
-
-        bright0 = "47435d"; # bright black (lighter Overlay)
-        bright1 = "ff98ba"; # bright red (lighter Love)
-        bright2 = "c5f9ff"; # bright green (lighter Foam)
-        bright3 = "ffeb9e"; # bright yellow (lighter Gold)
-        bright4 = "5b9ab7"; # bright blue (lighter Pine)
-        bright5 = "eed0ff"; # bright magenta (lighter Iris)
-        bright6 = "ffe5e3"; # bright cyan (lighter Rose)
-        bright7 = "fefcff"; # bright white (lighter Text)
-
-        flash = "f6c177"; # yellow (Gold)
+        # Nightfox colors for Xresources
+        # Style: nightfox
+        # Upstream: https://github.com/edeneast/nightfox.nvim/raw/main/extra/nightfox/nightfox.Xresources
+        background = "192330";
+        foreground = "cdcecf";
+        regular0 =  "393b44";
+        regular1 =  "c94f6d";
+        regular2 =  "81b29a";
+        regular3 =  "dbc074";
+        regular4 =  "719cd6";
+        regular5 =  "9d79d6";
+        regular6 =  "63cdcf";
+        regular7 =  "dfdfe0";
+        bright0 =  "575860";
+        bright1 =  "d16983";
+        bright2 = "8ebaa4";
+        bright3 = "e0c989";
+        bright4 = "86abdc";
+        bright5 = "baa1e2";
+        bright6 = "7ad5d6";
+        bright7 = "e4e4e5";
       };
 
       key-bindings = {
