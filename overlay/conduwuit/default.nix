d543d076984e4c5505d75e67778b7bdf979ac56c
cy <hi@cything.io>
2025-01-09 03:00:00 -0500

    build conduwuit with debug enabled

--- overlay/conduwuit/default.nix
+++ overlay/conduwuit/default.nix
@@ -5,13 +5,13 @@ final: prev: {
         args:
         old.rustPlatform.buildRustPackage (
           args
-          // rec {
+          // {
             version = "0.5.0-rc2";
             src = final.fetchFromGitHub {
               owner = "girlbossceo";
               repo = "conduwuit";
-              rev = "v${version}";
-              hash = "sha256-xnwqhU3yOIyWRrD/Pq3jmUHoNZSY8Ms9h8OTsZVYQ1g=";
+              rev = "8c74e35e7640a041c1f3496d82585e5240294352";
+              hash = "sha256-/2YD3TXT9pQ7oPEm9wDrq35afU88qukMIWqrBX5JyXg=";
             };
             doCheck = false;
             cargoHash = "sha256-ZenMTCEJrALKQnW7/eXqrhFj+BedE9i/rQZMsPHl8K0=";
@@ -20,6 +20,18 @@ final: prev: {
             ];
             # unstable has this set to "conduit"
             meta.mainProgram = "conduwuit";
+
+            buildFeatures = [
+              "brotli_compression"
+              "element_hacks"
+              "gzip_compression"
+              # "release_max_log_level" # without this feature to enable debug logging
+              "sentry_telemetry"
+              "systemd"
+              "zstd_compression"
+              "jemalloc"
+              "io_uring"
+            ];
           }
         );
     };
