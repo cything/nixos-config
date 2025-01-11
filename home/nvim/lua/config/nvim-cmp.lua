8dddc70a6c113418d295cc319ed10644dd0570ae
cy <hi@cything.io>
2025-01-10 21:43:17 -0500

    nvim: dont jump

--- home/nvim/lua/config/nvim-cmp.lua
+++ home/nvim/lua/config/nvim-cmp.lua
@@ -25,20 +25,20 @@ cmp.setup({
 		end),
 
 		["<C-n>"] = cmp.mapping(function(fallback)
-			if luasnip.locally_jumpable(1) then
-				luasnip.jump(1)
-			elseif cmp.visible() then
+			if cmp.visible() then
 				cmp.select_next_item()
+      elseif luasnip.locally_jumpable(1) then
+				luasnip.jump(1)
 			else
 				fallback()
 			end
 		end, { "i", "s" }),
 
 		["<C-p>"] = cmp.mapping(function(fallback)
-			if luasnip.locally_jumpable(-1) then
-				luasnip.jump(-1)
-			elseif cmp.visible() then
+			if cmp.visible() then
 				cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
