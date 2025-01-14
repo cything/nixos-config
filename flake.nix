a8ce6936a7c3988b8736d241a9e4588b97ae4ab5
cy <hi@cything.io>
2025-01-13 20:09:39 -0500

    use unstable-small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "cy's flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
