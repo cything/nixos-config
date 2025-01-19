02c356ec5170d96c533baa5553a5f4b7f6ce3c68
cy <cy@cy7.sh>
2025-01-19 03:59:33 -0500

    flake update and add nix-community cache

--- flake.nix
+++ flake.nix
@@ -38,10 +38,12 @@
     extra-substituters = [
       "https://cache.cything.io/central"
       "https://niri.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "central:uWhjva6m6dhC2hqNisjn2hXGvdGBs19vPkA1dPEuwFg="
       "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     builders-use-substitutes = true;
   };
