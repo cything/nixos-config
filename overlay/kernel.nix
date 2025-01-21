38b90fd28e4c5623c2cbd198d8c84055468c59e6
cy <cy@cy7.sh>
2025-01-21 04:04:08 -0500

    overlay zen

--- overlay/kernel.nix
+++ overlay/kernel.nix
@@ -1,7 +1,7 @@
 final: prev: let
   inherit (prev) lib;
 in {
-  linux_zen = prev.linux_zen.override (old: {
+  linuxKernels.kernels.linux_zen = prev.linuxKernels.kernels.linux_zen.override (old: {
     extraStructuredConfig = with lib.kernel; {
       CONFIG_SCHED_MUQSS = yes;
     };
