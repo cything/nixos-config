f10401c1870316ee91b215f284235cd6aa2ed594
cy <cy@cy7.sh>
2025-01-24 15:36:15 -0500

    init
    Signed-off-by: cy <cy@cy7.sh>

---
+++ home/yt/pancake.nix
@@ -0,0 +1,21 @@
+{
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ./common.nix
+  ];
+  home = {
+    username = "yt";
+    homeDirectory = "/home/yt";
+    stateVersion = "25.05";
+  };
+  programs.home-manager.enable = true;
+
+  systemd.user.startServices = "sd-switch";
+
+  home.packages = with pkgs; [
+    attic-server
+  ];
+}
