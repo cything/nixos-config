f0de5bb3fd513c5671bff148ae2a480512c9ee14
cy <cy@cy7.sh>
2025-02-22 12:57:00 -0500

    can't find editorconfig for some reason

--- home/codium.nix
+++ home/codium.nix
@@ -12,7 +12,6 @@
       with pkgs.open-vsx; [
         vscodevim.vim
         jnoortheen.nix-ide
-        editorconfig.editorconfig
         github.github-vscode-theme
         rust-lang.rust-analyzer
         shd101wyy.markdown-preview-enhanced
