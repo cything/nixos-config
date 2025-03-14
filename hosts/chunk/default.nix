60727812b81ea956b6c323979ef3df0fc8ae6d03
cy <cy@cy7.sh>
2025-03-14 00:55:53 -0400

    rm tor and update firewall stuff

--- hosts/chunk/default.nix
+++ hosts/chunk/default.nix
@@ -22,7 +22,6 @@
     ./forgejo.nix
     ./garage.nix
     ./tailscale.nix
-    ./tor.nix
   ];
 
   sops.age.keyFile = "/root/.config/sops/age/keys.txt";
@@ -101,22 +100,18 @@
           ${tc} qdisc del dev ens18 root || true
 
           # create HTB hierarchy
-          ${tc} qdisc add dev ens18 root handle 1: htb default 30
+          ${tc} qdisc add dev ens18 root handle 1: htb default 10
           ${tc} class add dev ens18 parent 1: classid 1:1 htb rate 100% ceil 100%
-          # tailscale
-          ${tc} class add dev ens18 parent 1:1 classid 1:10 htb rate 30% ceil 100%
-          # caddy
-          ${tc} class add dev ens18 parent 1:1 classid 1:20 htb rate 30% ceil 100%
           # rest
+          ${tc} class add dev ens18 parent 1:1 classid 1:10 htb rate 60% ceil 100%
+          # caddy
           ${tc} class add dev ens18 parent 1:1 classid 1:30 htb rate 40% ceil 100%
 
           # mark traffic
-          iptables -t mangle -A OUTPUT -m cgroup --path "system.slice/tailscaled.service" -j MARK --set-mark 1
-          iptables -t mangle -A OUTPUT -m cgroup --path "system.slice/caddy.service" -j MARK --set-mark 2
+          iptables -t mangle -A OUTPUT -m cgroup --path "system.slice/caddy.service" -j MARK --set-mark 3
 
           # route marked packets
-          ${tc} filter add dev ens18 parent 1: protocol ip prio 1 handle 1 fw flowid 1:10
-          ${tc} filter add dev ens18 parent 1: protocol ip prio 1 handle 2 fw flowid 1:20
+          ${tc} filter add dev ens18 parent 1: protocol ip prio 1 handle 3 fw flowid 1:30
         '';
     };
     interfaces.ens18 = {
