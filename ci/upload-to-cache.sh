c2c1ca6f3ac65aeb41f4860b977871e83dab70ab
cy <cy@cy7.sh>
2025-04-25 12:43:12 -0400

    test this cache

--- ci/upload-to-cache.sh
+++ ci/upload-to-cache.sh
@@ -5,4 +5,10 @@ set -eu
 set -f # disable globbing
 export IFS=' '
 echo "Uploading paths" $OUT_PATHS
-exec /nix/var/nix/profiles/default/bin/nix copy --to "s3://nixcache?endpoint=s3.cy7.sh&compression=zstd&parallel-compression=true" $OUT_PATHS
+
+# nix may run before rclone mount is done
+if [ -d "/mnt/nixcache" ]; then
+	exec /nix/var/nix/profiles/default/bin/nix copy --to "file:///mnt/nixcache&compression=zstd&parallel-compression=true" $OUT_PATHS
+else
+	echo "WARNING: skipping upload cause /mnt/nixcache does not exist"
+fi
