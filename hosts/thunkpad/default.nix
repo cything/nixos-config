ec9c046fa2843756ec81c893e689b5d630496137
cy <cy@cy7.sh>
2025-08-20 11:10:06 -0500

    thunkpad: fix wifi after hibernate

--- hosts/thunkpad/default.nix
+++ hosts/thunkpad/default.nix
@@ -1,12 +1,16 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-      ../common.nix
-      ../zsh.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ../common.nix
+    ../zsh.nix
+  ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -73,5 +77,36 @@
     lidSwitch = "suspend";
     powerKey = "hibernate";
   };
-}
 
+  # fix for wifi not work after resume from hibernation
+  # see https://wiki.archlinux.org/title/Lenovo_ThinkPad_T14s_(AMD)_Gen_3#Network_/_Wi-Fi
+  systemd.services.ath11k-suspend = {
+    enable = true;
+    before = [ "sleep.target" ];
+    wantedBy = [ "sleep.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${lib.getExe' pkgs.kmod "rmmod"} ath11k_pci";
+    };
+  };
+
+  systemd.services.ath11k-resume = {
+    enable = true;
+    after = [
+      "suspend.target"
+      "suspend-then-hibernate.target"
+      "hibernate.target"
+      "hybrid-sleep.target"
+    ];
+    wantedBy = [
+      "suspend.target"
+      "suspend-then-hibernate.target"
+      "hibernate.target"
+      "hybrid-sleep.target"
+    ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${lib.getExe' pkgs.kmod "modprobe"} ath11k_pci";
+    };
+  };
+}
