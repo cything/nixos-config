95fa584db1e6e690aa51b08db86c968508b899d4
cy <cy@cy7.sh>
2025-08-25 20:21:40 -0500

    chunk: fix backup

--- hosts/chunk/backup.nix
+++ hosts/chunk/backup.nix
@@ -5,12 +5,11 @@
 {
   my.backup = {
     enable = true;
-    jobName = "crashRsync";
     paths = [
       "/var/backup"
     ];
-    repo = "crash";
-    passFile = config.sops.secrets."borg/rsyncnet".path;
+    repo = "chunk";
+    passFile = config.sops.secrets."restic/zh5061".path;
     sshKeyFile = config.sops.secrets."rsyncnet/id_ed25519".path;
   };
 }
