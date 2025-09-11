28a6e2bff2eeb80dfd0824f10b1194269d9c0cc1
cy <cy@cy7.sh>
2025-09-11 17:15:05 -0500

    fix caddy hash

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -21,7 +21,7 @@ in
           # (still need the @ to pass nix config check)
           "github.com/caddy-dns/cloudflare@v0.2.2-0.20250506153119-35fb8474f57d"
         ];
-        hash = "sha256-1LgYTfzitHBd70kQzqU9G7EA8/SGkg5h19jrF2/dYtI=";
+        hash = "sha256-csX5HMFkC293U3gffI3Djvy5HkfzaaX7g6IdLow6cRs=";
       };
       logFormat = lib.mkForce "level INFO";
       acmeCA = "https://acme-v02.api.letsencrypt.org/directory";
